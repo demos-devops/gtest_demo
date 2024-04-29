2916f31b6bdbcbe5aab7d13a776cdec8c6068f41
alix.gao <alix.gao@qq.com>
2024-04-29 08:12:37 +0800

    feat: add comment
    Signed-off-by: alix.gao <alix.gao@qq.com>

--- run.sh
+++ run.sh
@@ -19,3 +19,14 @@ cd coverage
 lcov --directory ../build --capture --output-file ut-coverage.info
 lcov -e ut-coverage.info '*source*' --output-file ut-cov-filtered.info
 genhtml ut-cov-filtered.info --output-directory ut-coverage-report
+
+# pipeline
+#    publishHTML([
+#        allowMissing: false,
+#        alwaysLinkToLastBuild: true,
+#        keepAll: true,
+#        reportDir: 'coverage',
+#        reportFiles: 'coverage-report/index.html, ut-coverage-report/index.html, it-coverage-report/index.html, st-coverage-report/index.html',
+#        reportName: 'Test Coverage Report',
+#        reportTitles: 'all-test-coverage, ut-coverage, it-coverage, st-coverage'
+#    ])
